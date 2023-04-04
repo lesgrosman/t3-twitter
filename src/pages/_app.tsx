7b7c99787df52b48e1eb07d75503df90e83b7938
Glib Konstantinov <andreyagasov@gmail.com>
2023-04-04 19:58:38 +0200

    add modal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { SessionProvider } from 'next-auth/react'
 import { api } from '@/utils/api'
 
 import '@/styles/globals.css'
+import Layout from '@/components/Layout'
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -13,7 +14,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
   )
 }
